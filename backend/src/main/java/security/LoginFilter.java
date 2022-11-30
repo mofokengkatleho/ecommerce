b8f252650624ef2297cf94066dafdcb68f82fc54
katleho <katlehomofokeng@protonmail.com>
2022-11-30 08:01:50 +0200

    create fields and constructor for LoginFilter

--- backend/src/main/java/security/LoginFilter.java
+++ backend/src/main/java/security/LoginFilter.java
@@ -4,8 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static security.Constants.TOKEN_URL;
+
+
 public class LoginFilter  extends  UsernamePasswordAuthenticationFilter{
     private final AuthenticationManager authenticationManager;
     private final JwtManager tokenManager;
     private final ObjectMapper mapper;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JwtManager tokenManager,
+                       ObjectMapper mapper) {
+        this.authenticationManager = authenticationManager;
+        this.tokenManager = tokenManager;
+        this.mapper = mapper;
+        super.setFilterProcessesUrl(TOKEN_URL);
+    }
 }
