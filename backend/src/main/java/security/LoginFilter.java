4fdf9a6a90041859b2583c0124e4c4a47a9fbd16
katleho <katlehomofokeng@protonmail.com>
2022-11-30 08:39:48 +0200

    override attemptAuthentication method

--- backend/src/main/java/security/LoginFilter.java
+++ backend/src/main/java/security/LoginFilter.java
@@ -1,8 +1,20 @@
 package security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.server.MethodNotAllowedException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
 
 import static security.Constants.TOKEN_URL;
 
@@ -18,5 +30,24 @@ public class LoginFilter  extends  UsernamePasswordAuthenticationFilter{
         this.tokenManager = tokenManager;
         this.mapper = mapper;
         super.setFilterProcessesUrl(TOKEN_URL);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        if (!req.getMethod().equals(HttpMethod.POST.name())) {
+            throw new MethodNotAllowedException(req.getMethod(), List.of(HttpMethod.POST));
+        }
+        try (InputStream is = req.getInputStream()) {
+            SignInReq user = new ObjectMapper().readValue(is, SignInReq.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUsername(),
+                            user.getPassword(),
+                            Collections.emptyList())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
