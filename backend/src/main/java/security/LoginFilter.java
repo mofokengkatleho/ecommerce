3914c73619dc4706e4e025855336b7dc7cc4f91c
katleho <katlehomofokeng@protonmail.com>
2022-11-30 07:55:53 +0200

    add Constants

--- backend/src/main/java/security/LoginFilter.java
+++ backend/src/main/java/security/LoginFilter.java
@@ -1,6 +1,11 @@
 package security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class LoginFilter  extends  UsernamePasswordAuthenticationFilter{
+    private final AuthenticationManager authenticationManager;
+    private final JwtManager tokenManager;
+    private final ObjectMapper mapper;
 }
